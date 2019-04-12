7af637970079dd57a183240fd25ac994b146565a
Aj Gill <ajgill@ucdavis.edu>
2019-04-12 14:54:49 -0700

    Initial Commit

---
+++ Sort Array By Parity/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sort Array By Parity
+//
+//  Created by Aj Gill on 4/12/19.
+//  Copyright © 2019 Aj Gill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
